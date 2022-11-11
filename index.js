7340a4b7a099fccfcf4596b6d78af0a36acbc152
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-11 11:38:37 +0200

    server response

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const http = require('http');
 const PORT = 3000;
 
 const requestListener = (req, res) => {
-    const {url, method} = req;
-    console.log(url, method);
-   // console.log(res);
+    // const {url, method} = req;
+    // console.log(url, method);
+    res.statusCode = 200;
+    res.end('Hello from server');
 }
 
 const server = http.createServer(requestListener);
