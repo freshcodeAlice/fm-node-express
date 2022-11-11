eb48ae49a44f8fb421426d3f5e02c8634d6d2a3c
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-11 12:00:49 +0200

    Task

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ const PORT = 3000;
 const requestListener = async (req, res) => {
     const {url} = req;
     if ( url === '/index.html') {
-       const data = await fs.readFile('./views/index.html', 'utf-8');
-        res.end(data);
+        try {
+            const data = await fs.readFile('./views/index.html', 'utf-8');
+            res.end(data);
+        } catch(error) {
+            res.statusCode = 404;
+            res.end();
+        }
+
+    } else if( url === '/style.css') {
+        //TODO: прочитати з файлової системи файл style.css і віддати його у відповідь на запит
     } else {
-        res.statusCode = 404;
+        res.statusCode = 400;
         res.end();
     }
 }
