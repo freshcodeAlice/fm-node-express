e3921ac18c69b6742b9f5b6df697582db361beca
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-11 11:53:42 +0200

    refactor async/await

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const fs = require('fs/promises');
 
 const PORT = 3000;
 
-const requestListener = (req, res) => {
+const requestListener = async (req, res) => {
     const {url} = req;
     if ( url === '/index.html') {
-        fs.readFile('./views/index.html', 'utf-8')
-        .then((data) => {
-            res.end(data);
-        })
+       const data = await fs.readFile('./views/index.html', 'utf-8');
+        res.end(data);
     } else {
         res.statusCode = 404;
         res.end();
