6deed0485b2fdc9587676b66ef1ae93590ba0050
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-11 11:51:42 +0200

    server send html

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const http = require('http');
+const fs = require('fs/promises');
 
 
 const PORT = 3000;
 
 const requestListener = (req, res) => {
-    // const {url, method} = req;
-    // console.log(url, method);
-    res.statusCode = 200;
-    res.end('Hello from server');
+    const {url} = req;
+    if ( url === '/index.html') {
+        fs.readFile('./views/index.html', 'utf-8')
+        .then((data) => {
+            res.end(data);
+        })
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 }
 
 const server = http.createServer(requestListener);
