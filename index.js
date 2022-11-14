1213de48553203d2255eb561808161d3d65aa9c7
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-14 12:02:49 +0200

    task

--- index.js
+++ index.js
@@ -8,23 +8,9 @@ const PORT = 3000;
 const bodyParser = express.json();
 
 app.post('/user', bodyParser, validateUser, UserController.createUser);
+// створити роут для отримання всіх юзерів
 
 app.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}`);
 });
 
-
-/*
-Зареєструвати юзера
-
-+1. Прийняти запит на певний endpoint
-+2. Розпарсити дані, які прийшли з запитом.
-+3. Перевірити (валідувати) дані.
-+4. Зберегти ці дані.
-6. Підготувати дані юзера у відповідь. 
-7. Надіслати підготовлені дані у відповідь на запит.
-
-
-
-
-*/
