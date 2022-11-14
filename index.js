32fb1f446b2f9ce7f1c99846c7597d0673dbaeff
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-14 09:59:48 +0200

    Express welcome

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-const http = require('http');
-const fs = require('fs/promises');
-
-
+const express = require('express');
+const app = express();
 const PORT = 3000;
 
-const requestListener = async (req, res) => {
-    const {url, method} = req;
-    if (method === 'GET'){
-        if ( url === '/') {
-            try {
-                const data = await fs.readFile('./views/index.html', 'utf-8');
-                res.end(data);
-            } catch(error) {
-                res.statusCode = 404;
-                res.end();
-            }
+app.get('/', (req, res)=>{
+    res.send('Hello, world');
+})
 
-        } else if( url === '/style.css') {
-            try {
-                const data = await fs.readFile('./views/style.css', 'utf-8');
-                res.end(data);
-            } catch(error) {
-                res.statusCode = 404;
-                res.end();
-            }
-        } else {
-            res.statusCode = 400;
-            res.end();
-        }
-    } else if(method === 'POST') {
-        if(url === '/user') {
-            let jsonString = '';
-          req.on('data', (chunk) => {
-            jsonString += chunk
-          });
-          req.on('end', () => {
-            const user = JSON.parse(jsonString);
-            console.log(user);
-          });
-        }
-    } else {
-        res.statusCode = 400;
-        res.end();
-    }
-}
 
-const server = http.createServer(requestListener);
 
-server.listen(PORT);
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`);
+});
